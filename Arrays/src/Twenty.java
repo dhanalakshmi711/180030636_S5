d1486b235ba1803e3736fd0d3fd1d1e2b15fbf00
dhanalakshmi711 <dhanalakshmi7pula@gmail.com>
2021-05-11 10:52:55 +0530

    commit

---
+++ Arrays/src/Twenty.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.*;
+public class Twenty {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      a[i]=sc.nextInt();
+    }
+    int s=0;
+    float av=0;
+    for(int i=0;i<n;i++)
+    {
+      s=s+a[i];
+    }
+    av=s/n;
+    System.out.println("Sum is "+s);
+    System.out.println("Avg is "+av);
+  }
+
+}
+
